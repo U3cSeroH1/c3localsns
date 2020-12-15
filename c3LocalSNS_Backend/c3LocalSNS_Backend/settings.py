63e09efc5ca05dc3a49440412a982e8320979c9d
U3cSeroH1 <unkio0602@gmail.com>
2020-12-15 23:26:16 +0900

    「ふぃっくす」うんこおおおおおおおおおおおおおおおおおおおおおおおおおおふぉおおおおおおおおおおお

--- c3LocalSNS_Backend/c3LocalSNS_Backend/settings.py
+++ c3LocalSNS_Backend/c3LocalSNS_Backend/settings.py
@@ -186,6 +186,14 @@ JWT_AUTH = {
     'JWT_VERIFY_EXPIRATION': False,
 }
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+}
+
 SOCIALACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_ADAPTER = 'customDiscordProvider.adapter.MyAccountAdapter'
 
